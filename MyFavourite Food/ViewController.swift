3509e0bc0292ecbf34af0fac3f71453058c50fe7
yushuwu <wu.yushu1@northeastern.edu>
2022-09-23 14:28:15 -0700

    Assignment2

--- MyFavourite Food/ViewController.swift
+++ MyFavourite Food/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var tblView: UITableView!
+    let food = ["Tofu", "Beef", "Pizza", "Dumplings", "Pasta"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return food.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = food[indexPath.row]
+        return cell
     }
 
 
