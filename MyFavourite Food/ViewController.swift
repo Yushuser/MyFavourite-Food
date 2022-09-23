2ac6a71e7a6d2109f8007a63421a0748225e7a1f
yushuwu <wu.yushu1@northeastern.edu>
2022-09-23 14:18:44 -0700

    Initial Commit

---
+++ MyFavourite Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavourite Food
+//
+//  Created by YUSHU WU on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
